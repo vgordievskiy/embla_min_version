2c44aed79b98d370971e37471f7b2e310e0b15a7
gardi <v.gordievskiy@gmail.com>
2015-04-07 00:31:10 +0000

    fix

--- bin/main.dart
+++ bin/main.dart
@@ -5,14 +5,16 @@ import 'package:redstone_mapper/plugin.dart';
 import 'package:BMSrv/BMSrv.dart';
 import 'package:BMSrv/Events/EventBus.dart';
 import 'package:BMSrv/Utils/DbAdapter.dart';
+import 'package:BMSrv/Controllers/UserManager.dart';
 
 main() {
   Init().then((var res){
     app.addPlugin(getMapperPlugin());
     app.addModule(new Module()..bind(DBAdapter));
     app.addModule(new Module()..bind(EventSys));
+    app.addModule(new Module()..bind(UserManager));
     app.setupConsoleLog();
 
-    app.start(port: 8000);
+    app.start(port: 8001);
   });
 }
--- lib/BMSrv.dart
+++ lib/BMSrv.dart
@@ -1,6 +1,11 @@
 library BMSrv;
 
 import 'dart:async';
+/*----For corrent initialize RedstoneDart--------*/
+import 'package:BMSrv/Services/Cors_Auth.dart';
+import 'package:BMSrv/Services/LoginService.dart';
+import 'package:BMSrv/Services/UserService.dart';
+/*----------------------------------------------*/
 
 import 'package:BMSrv/Storage/BMOntology.dart' as Ontology;
 import 'package:BMSrv/Storage/SemplexStorage.dart';
