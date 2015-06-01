8d573dd42780f541a330b539b8edc3c08d6fd4cd
v.gordiedievskiy <v.gordievskiy@gmail.com>
2015-06-01 14:10:13 +0300

    init RealEstateService

---
+++ lib/Services/RealEstateService.dart
@@ -0,0 +1,31 @@
+library BMSrv.RealEstateService;
+import 'dart:async';
+import 'package:dart_orm/dart_orm.dart' as ORM;
+import 'package:redstone/server.dart' as app;
+import 'package:redstone_mapper/plugin.dart';
+import 'package:uuid/uuid.dart';
+import 'package:logging/logging.dart';
+
+import 'package:BMSrv/Events/Event.dart';
+import 'package:BMSrv/Utils/DbAdapter.dart';
+import 'package:BMSrv/Models/User.dart';
+import 'package:BMSrv/Models/RealEstate.dart';
+
+bool _isEmpty(String value) => value == "";
+
+Future<User> _getUser(String name) async {
+  ORM.Find find = new ORM.Find(User)..where(new ORM.Equals('userName', name));
+  List foundUsers = await find.execute();
+  return foundUsers[0];
+}
+
+@app.Group("/realestate")
+class RealEstateService {
+  DBAdapter _Db;
+  Uuid _Generator;
+  final log = new Logger("BMSrv.Services.RealEstateService");
+  RealEstateService(DBAdapter this._Db)
+  {
+    _Generator = new Uuid();
+  }
+}
