fe7db440c16b9fde8130f7a363679d7226f03f86
v.gordiedievskiy <v.gordievskiy@gmail.com>
2015-06-04 14:55:00 +0300

    refactoring

--- lib/Services/RealEstateService.dart
+++ lib/Services/RealEstateService.dart
@@ -1,27 +1,27 @@
 library BMSrv.RealEstateService;
 import 'dart:async';
+
+import 'package:BMSrv/Events/Event.dart';
+import 'package:BMSrv/Models/JsonWrappers/ObjectDeal.dart';
+import 'package:BMSrv/Models/JsonWrappers/RECommercial.dart';
+import 'package:BMSrv/Models/JsonWrappers/RELand.dart';
+import 'package:BMSrv/Models/JsonWrappers/REPrivate.dart';
+import 'package:BMSrv/Models/RealEstate/RealEstate.dart';
+import 'package:BMSrv/Models/User.dart';
+import 'package:BMSrv/Utils/DbAdapter.dart';
 import 'package:dart_orm/dart_orm.dart' as ORM;
+import 'package:logging/logging.dart';
 import 'package:redstone/server.dart' as app;
 import 'package:redstone_mapper/plugin.dart';
 import 'package:uuid/uuid.dart';
-import 'package:logging/logging.dart';
-
-import 'package:BMSrv/Events/Event.dart';
-import 'package:BMSrv/Utils/DbAdapter.dart';
-import 'package:BMSrv/Models/User.dart';
-import 'package:BMSrv/Models/RealEstate/RealEstate.dart';
-import 'package:BMSrv/Models/JsonWrappers/RECommercial.dart';
-import 'package:BMSrv/Models/JsonWrappers/REPrivate.dart';
-import 'package:BMSrv/Models/JsonWrappers/RELand.dart';
-import 'package:BMSrv/Models/JsonWrappers/ObjectDeal.dart';
-
-bool _isEmpty(String value) => value == "";
 
 Future<User> _getUser(String name) async {
   ORM.Find find = new ORM.Find(User)..where(new ORM.Equals('userName', name));
   List foundUsers = await find.execute();
   return foundUsers[0];
 }
+
+bool _isEmpty(String value) => value == "";
 
 @app.Group("/realestate")
 class RealEstateService {
@@ -31,30 +31,6 @@ class RealEstateService {
   RealEstateService(DBAdapter this._Db)
   {
     _Generator = new Uuid();
-  }
-  
-  @app.Route("/private", methods: const[app.POST])
-  create_private(@app.Body(app.FORM) Map data) async {
-    if (_isEmpty(data['objectName']))
-    {
-      throw new app.ErrorResponse(403, {"error": "data empty"});
-    }
-
-    REPrivate object = new REPrivate.Dummy();
-    
-    object.objectName = data['objectName'];
-
-    var exception = null;
-
-    var saveResult = await object.save().catchError((var error){
-      exception = error;
-    });
-    
-    if (exception != null) {
-      return exception;
-    } else {
-      return object.id;
-    }
   }
   
   @app.Route("/commercial", methods: const[app.POST])
@@ -105,25 +81,28 @@ class RealEstateService {
     }
   }
   
-  @app.DefaultRoute()
-  @Encode()
-  Future<List<dynamic>> getAllObjects() async {
-    List<dynamic> ret = new List();
-    ret.addAll(await getAllPrivate());
-    ret.addAll(await getAllCommercial());
-    ret.addAll(await getAllLand());
-    return ret;
-  }
-  
-  @app.Route("/private", methods: const[app.GET])
-  @Encode()
-  Future<List<REPrivateWrapper>> getAllPrivate() async {
-    ORM.Find find = new ORM.Find(REPrivate);
-    List<REPrivateWrapper> ret = new List();
-    for(REPrivate obj in await find.execute()) {
-      ret.add(await REPrivateWrapper.Create(obj));
+  @app.Route("/private", methods: const[app.POST])
+  create_private(@app.Body(app.FORM) Map data) async {
+    if (_isEmpty(data['objectName']))
+    {
+      throw new app.ErrorResponse(403, {"error": "data empty"});
     }
-    return ret;
+
+    REPrivate object = new REPrivate.Dummy();
+    
+    object.objectName = data['objectName'];
+
+    var exception = null;
+
+    var saveResult = await object.save().catchError((var error){
+      exception = error;
+    });
+    
+    if (exception != null) {
+      return exception;
+    } else {
+      return object.id;
+    }
   }
   
   @app.Route("/commercial", methods: const[app.GET])
@@ -146,6 +125,49 @@ class RealEstateService {
       ret.add(await RELandWrapper.Create(obj));
     }
     return ret;
+  }
+  
+  @app.DefaultRoute()
+  @Encode()
+  Future<List<dynamic>> getAllObjects() async {
+    List<dynamic> ret = new List();
+    ret.addAll(await getAllPrivate());
+    ret.addAll(await getAllCommercial());
+    ret.addAll(await getAllLand());
+    return ret;
+  }
+  
+  @app.Route("/private", methods: const[app.GET])
+  @Encode()
+  Future<List<REPrivateWrapper>> getAllPrivate() async {
+    ORM.Find find = new ORM.Find(REPrivate);
+    List<REPrivateWrapper> ret = new List();
+    for(REPrivate obj in await find.execute()) {
+      ret.add(await REPrivateWrapper.Create(obj));
+    }
+    return ret;
+  }
+  
+  @app.Route("/commercial/:id", methods: const[app.GET])
+  @Encode()
+  Future<RECommercialWrapper> getCommercialById(String id) async {
+    ORM.FindOne find = new ORM.FindOne(RECommercial)..whereEquals('id', id);
+    var ret = await find.execute();
+    if (ret == null) {
+      return  new app.ErrorResponse(404, {"error": "not found object"});
+    }
+    return RECommercialWrapper.Create(ret);
+  }
+  
+  @app.Route("/land/:id", methods: const[app.GET])
+  @Encode()
+  Future<RELandWrapper> getLandById(String id) async {
+    ORM.FindOne find = new ORM.FindOne(RELand)..whereEquals('id', id);
+    var ret = await find.execute();
+    if (ret == null) {
+      return  new app.ErrorResponse(404, {"error": "not found object"});
+    }
+    return RELandWrapper.Create(ret);
   }
   
   @app.Route("/private/:id", methods: const[app.GET])
@@ -174,27 +196,5 @@ class RealEstateService {
       ret.add(await ObjectDealWrapper.Create(deal));
     }
     return ret;
-  }
-  
-  @app.Route("/commercial/:id", methods: const[app.GET])
-  @Encode()
-  Future<RECommercialWrapper> getCommercialById(String id) async {
-    ORM.FindOne find = new ORM.FindOne(RECommercial)..whereEquals('id', id);
-    var ret = await find.execute();
-    if (ret == null) {
-      return  new app.ErrorResponse(404, {"error": "not found object"});
-    }
-    return RECommercialWrapper.Create(ret);
-  }
-  
-  @app.Route("/land/:id", methods: const[app.GET])
-  @Encode()
-  Future<RELandWrapper> getLandById(String id) async {
-    ORM.FindOne find = new ORM.FindOne(RELand)..whereEquals('id', id);
-    var ret = await find.execute();
-    if (ret == null) {
-      return  new app.ErrorResponse(404, {"error": "not found object"});
-    }
-    return RELandWrapper.Create(ret);
   }
 }
