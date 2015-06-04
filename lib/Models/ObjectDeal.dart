76b9df429ab1e8cb08965d205b5ef64ef411eb04
v.gordiedievskiy <v.gordievskiy@gmail.com>
2015-06-04 13:48:53 +0300

    refactoring

--- lib/Models/ObjectDeal.dart
+++ lib/Models/ObjectDeal.dart
@@ -2,14 +2,14 @@ library BMSrv.Models.ObjectDeal;
 
 import 'dart:async';
 
-import 'package:observe/observe.dart';
+import 'package:BMSrv/Models/RealEstate/REPrivate.dart';
+import 'package:BMSrv/Models/RealEstate/RealEstate.dart';
+import 'package:BMSrv/Models/User.dart';
+import 'package:BMSrv/Storage/BMOntology.dart';
+import 'package:BMSrv/Storage/SemplexStorage.dart';
 import 'package:dart_orm/dart_orm.dart' as ORM;
 import 'package:logging/logging.dart';
-import 'package:BMSrv/Storage/SemplexStorage.dart';
-import 'package:BMSrv/Storage/BMOntology.dart';
-import 'package:BMSrv/Models/User.dart';
-import 'package:BMSrv/Models/RealEstate/RealEstate.dart';
-import 'package:BMSrv/Models/RealEstate/REPrivate.dart';
+import 'package:observe/observe.dart';
 
 @ORM.DBTable('user_object_deal')
 class ObjectDeal extends OntoEntity {
@@ -19,6 +19,30 @@ class ObjectDeal extends OntoEntity {
   static const int Land = 2;
   
   Logger _log;
+  @ORM.DBField()
+  @ORM.DBFieldType('UNIQUE')
+  String ontoId;
+  
+  @ORM.DBField()
+  @ORM.DBFieldPrimaryKey()
+  @ORM.DBFieldType('SERIAL')
+  int id;
+  
+  @ORM.DBField()
+  int objectId;
+  
+  @ORM.DBField()
+  int userId;
+  
+  @ORM.DBField()
+  int type;
+  
+  @ORM.DBField()
+  bool isPending;
+  
+  @ORM.DBField()
+  double part;
+  
   ObjectDeal() {
     InitOnto("ObjectDeal");
     initLog();
@@ -30,15 +54,6 @@ class ObjectDeal extends OntoEntity {
       });*/
       OntoIndivid.Get(ind);
     });
-  }
-  
-  ObjectDeal.DummyPrivate(User user, REPrivate object, double _part) {
-    InitOnto("ObjectDeal");
-    userId = user.id;
-    objectId = object.id;
-    isPending = true;
-    part = _part;
-    initData(userId, object.id, Private);
   }
   
   ObjectDeal.DummyCommercial(User user, RECommercial object, double _part) {
@@ -58,69 +73,27 @@ class ObjectDeal extends OntoEntity {
     part = _part;
     initData(userId, object.id, Land);
   }
-  
-  initData(int UserId, int REId, int typeId) {
-    userId = UserId;
-    objectId = REId;
-    type = typeId;
-  }
-  
-  initLog() async {
-    _log = new Logger("BMSrv.ObjectDeal_$id");
-  }
-  
-  static Future<ObjectDeal> Get(String id) {
-    ORM.FindOne findOneItem = new ORM.FindOne(ObjectDeal)
-                                  ..whereEquals('id', id);
-    if (findOneItem != null) {
-      return findOneItem.execute();
-    }
-    throw "not found ${id}";
-  }
-  
-  @ORM.DBField()
-  @ORM.DBFieldType('UNIQUE')
-  String ontoId;
-  
-  @override
-  Future<bool> save() async {
-    if (this.id == null) {
-      try {
-        bool res = await super.save();
-        if (res == true) {
-          this.ontoId = $.EntityName;
-          return super.save();
-        }
-        return res;
-      } catch(error) { throw error; }
-    } else {
-      return super.save();
-    }
-  }
-  
-  @ORM.DBField()
-  @ORM.DBFieldPrimaryKey()
-  @ORM.DBFieldType('SERIAL')
-  int id;
 
-  @ORM.DBField()
-  int objectId;
+  ObjectDeal.DummyPrivate(User user, REPrivate object, double _part) {
+    InitOnto("ObjectDeal");
+    userId = user.id;
+    objectId = object.id;
+    isPending = true;
+    part = _part;
+    initData(userId, object.id, Private);
+  }
   
-  @ORM.DBField()
-  int userId;
-  
-  @ORM.DBField()
-  int type;
-  
-  @ORM.DBField()
-  bool isPending;
-  
-  @ORM.DBField()
-  double part;
-  
-  Future<User> GetUser() {
-    ORM.FindOne find = new ORM.FindOne(User)..whereEquals('id', userId);
-    return find.execute();
+  String get TypeName {
+    switch(type) {
+      case Private :
+        return "private";
+      case Commercial :
+        return "commercial";
+      case Land :
+        return "land";
+    }
+    assert(false);
+    return "";
   }
   
   Future<dynamic> GetObject() {
@@ -140,20 +113,47 @@ class ObjectDeal extends OntoEntity {
     return find.execute();
   }
   
-  String get TypeName {
-    switch(type) {
-      case Private :
-        return "private";
-      case Commercial :
-        return "commercial";
-      case Land :
-        return "land";
-    }
-    assert(false);
-    return "";
+  Future<User> GetUser() {
+    ORM.FindOne find = new ORM.FindOne(User)..whereEquals('id', userId);
+    return (find.execute() as Future<User>);
   }
-
+  
+  initData(int UserId, int REId, int typeId) {
+    userId = UserId;
+    objectId = REId;
+    type = typeId;
+  }
+  
+  initLog() async {
+    _log = new Logger("BMSrv.ObjectDeal_$id");
+  }
+  
+  @override
+  Future<bool> save() async {
+    if (this.id == null) {
+      try {
+        bool res = await super.save();
+        if (res == true) {
+          this.ontoId = $.EntityName;
+          return super.save();
+        }
+        return res;
+      } catch(error) { throw error; }
+    } else {
+      return super.save();
+    }
+  }
+  
   String toString(){
     return 'ObjectDeal { id: $id, objectId: $objectId userId: $userId, type: $type}';
+  }
+
+  static Future<ObjectDeal> Get(String id) {
+    ORM.FindOne findOneItem = new ORM.FindOne(ObjectDeal)
+                                  ..whereEquals('id', id);
+    if (findOneItem != null) {
+      return (findOneItem.execute() as Future<ObjectDeal>);
+    }
+    throw "not found ${id}";
   }
 }
--- lib/Models/RealEstate/RealEstate.dart
+++ lib/Models/RealEstate/RealEstate.dart
@@ -10,8 +10,27 @@ import 'package:observe/observe.dart';
 import 'package:dart_orm/dart_orm.dart' as ORM;
 import 'package:BMSrv/Storage/SemplexStorage.dart';
 import 'package:BMSrv/Storage/BMOntology.dart';
+import 'package:BMSrv/Models/ObjectDeal.dart';
 import 'package:logging/logging.dart';
 
 abstract class RealEstateBase {
   static OntoClass OwnerClass = GetOntology().GetClass("RealEstate");
+  
+  int id;
+  
+  Future<List<ObjectDeal>> _getParts(bool isPending) async {
+    ORM.Find find = new ORM.Find(ObjectDeal)
+                            ..where(new ORM.Equals('objectId', this.id)
+                            .and(new ORM.Equals('isPending', isPending)));
+    return find.execute();
+  }
+  
+  Future<List<ObjectDeal>> GetPengindParts() async {
+    return _getParts(true);
+  }
+  
+  Future<List<ObjectDeal>> GetApprovedParts() async {
+    return _getParts(false);
+  }
+  
 }
