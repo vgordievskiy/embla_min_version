50def328089847f40394d8fbe3c71a939febc8c0
gardi <v.gordievskiy@gmail.com>
2015-08-11 00:33:06 +0000

    init generic estate wrapper

---
+++ lib/Models/JsonWrappers/REstate.dart
@@ -0,0 +1,40 @@
+library BMSrv.Models.JsonWrappers.REstate;
+import 'dart:async';
+import 'package:redstone_mapper/mapper.dart';
+import 'package:redstone_mapper/plugin.dart';
+
+import 'package:BMSrv/Models/RealEstate/RECommercial.dart';
+import 'package:BMSrv/Models/RealEstate/REPrivate.dart';
+import 'package:BMSrv/Models/RealEstate/RELand.dart';
+
+Map<Type, String> _converter = {
+  RECommercial : "commercial",
+  REPrivate : "private",
+  RELand : "land"
+};
+
+@Decode()
+class REstateWrapper {
+
+  static Future<REstateWrapper> Create(var object) async 
+  {
+    REstateWrapper ret = new REstateWrapper();
+    ret.id = object.id;
+    ret.type = _converter[object.runtimeType];
+    ret.objectName = object.objectName;
+    ret.Geo = await object.GetGeometry();
+    return ret;
+  }
+
+  @Field()
+  int id;
+  
+  @Field()
+  String type;
+
+  @Field()
+  String objectName;
+  
+  @Field()
+  Map<String, dynamic> Geo;
+}
