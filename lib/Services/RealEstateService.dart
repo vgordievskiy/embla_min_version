f09c54229a9a9cff2fb6f35e3a9d6113a166af3c
gardi <v.gordievskiy@gmail.com>
2015-08-16 22:10:56 +0000

    update Room

--- lib/Services/RealEstateService.dart
+++ lib/Services/RealEstateService.dart
@@ -128,7 +128,7 @@ class RealEstateService {
     }
   }
   
-  @app.Route("/:type/:id/rooms", methods: const [app.POST])
+  @app.Route("/:type/:id/rooms", methods: const [app.PUT])
   create_room(String type, String id, @app.Body(app.FORM) Map data) async {
     if (_isEmpty(data['objectName']) && _isEmpty(data['objectGeom'])
         && _isEmpty(data['square'])) {
