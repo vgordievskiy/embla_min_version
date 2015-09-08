6145613bcea1370d245bfd48a9072bb764bdf874
v.gordiedievskiy <v.gordievskiy@gmail.com>
2015-09-08 20:23:04 +0300

    add JSON Wrapper for REMetaData

---
+++ lib/Models/JsonWrappers/REMetaData.dart
@@ -0,0 +1,26 @@
+library BMSrv.Models.JsonWrappers.REMetaData;
+import 'dart:async';
+import 'package:redstone_mapper/mapper.dart';
+import 'package:redstone_mapper/plugin.dart';
+
+import 'package:BMSrv/Models/RealEstate/REMetaData.dart';
+
+@Decode()
+class REMetaDataWrapper {
+  
+  static Future<REMetaDataWrapper> Create(List<REMetaData> data) async 
+  {
+    REMetaDataWrapper ret = new REMetaDataWrapper();
+    
+    ret.data = new Map();
+    
+    for(REMetaData item in data) {
+      ret.data[item.name] = item.data;
+    }
+    
+    return ret;
+  }
+
+  @Field()
+  Map<String, dynamic> data;
+}
