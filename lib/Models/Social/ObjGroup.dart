6192e7fc751aeeb423deaa886e66d662614397dd
gardi <v.gordievskiy@gmail.com>
2015-09-13 16:58:04 +0000

    init Social.ObjGroup

--- lib/Models/Social/ObjGroup.dart
+++ lib/Models/Social/ObjGroup.dart
@@ -4,17 +4,49 @@ import 'dart:async';
 import 'dart:collection';
 import 'dart:convert';
 
-import 'package:BMSrv/Models/RealEstate/RealEstate.dart';
 import 'package:SrvCommon/SrvCommon.dart';
 import 'package:dart_orm/dart_orm.dart' as ORM;
 import 'package:logging/logging.dart';
 import 'package:observe/observe.dart';
+
+import 'package:BMSrv/Models/RealEstate/RealEstate.dart';
+import 'package:BMSrv/Models/User.dart';
+
+class ObjGroupUtils {
+  
+  static Future<ObjGroup> getForObject(RealEstateBase obj) async {
+    ORM.FindOne find = new ORM.FindOne(ObjGroup);
+    ORM.Condition cond = new ORM.Equals('objectType', ReUtils.type2Int(obj.Type));
+    cond.and(new ORM.Equals('objId', obj.id));
+    return find.execute();
+  }
+  
+  static Future<bool> addUserToGroup(RealEstateBase obj, User user) async {
+    ObjGroup grp = await getForObject(obj);
+    return grp.addUser(user.id);
+  }
+  
+  static Future<bool> createGroup(RealEstateBase obj) {
+    ObjGroup grp = new ObjGroup.Dummy();
+    grp.objectType = ReUtils.type2Int(obj.Type);
+    grp.objId = obj.id;
+    return grp.save();
+  }
+}
 
 @ORM.DBTable('users_social_group_by_object')
 class ObjGroup extends ORM.Model with Observable  {
   static List<int> _converter(Map<String, dynamic> value) {
     assert(value.containsKey('users'));
     return value['users']; 
+  }
+  
+  ObjGroup(): super() {
+    
+  }
+  
+  ObjGroup.Dummy(): super() {
+    _data = [];
   }
   
   @ORM.DBField()
@@ -34,7 +66,7 @@ class ObjGroup extends ORM.Model with Observable  {
     
   List<int> get Users => (_data as List<int>);
   
-  Future addUser(int userId) async {
+  Future<bool> addUser(int userId) async {
     if (Users == null) _data = [];
     List<int> value = Users;
     value.add(userId);
@@ -42,7 +74,7 @@ class ObjGroup extends ORM.Model with Observable  {
     return save();
   }
   
-  Future removeUser(int userId) async {
+  Future<bool> removeUser(int userId) async {
     List<int> value = Users;
     value.remove(userId);
     _data = value;
