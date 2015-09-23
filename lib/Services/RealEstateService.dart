5af147767d3b82b599f4fd475dcecfd504d792a1
v.gordiedievskiy <v.gordievskiy@gmail.com>
2015-09-23 18:32:48 +0300

    fix

--- lib/Services/RealEstateService.dart
+++ lib/Services/RealEstateService.dart
@@ -27,7 +27,7 @@ Future<User> _getUser(String name) async {
   return foundUsers[0];
 }
 
-bool _isEmpty(String value) => value == null || value == "";
+bool _isEmpty(String value) => value == null;
 
 class HelperObjectConverter<JsonWrapper> {
   ClassMirror _Class = reflectClass(JsonWrapper);
