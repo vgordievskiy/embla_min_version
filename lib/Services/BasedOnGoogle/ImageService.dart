1a56b4eec058c6a671d5b758b5ff8d1b4e73d671
gardi <v.gordievskiy@gmail.com>
2016-01-31 00:31:58 +0000

    updt

--- lib/Services/BasedOnGoogle/ImageService.dart
+++ lib/Services/BasedOnGoogle/ImageService.dart
@@ -19,33 +19,33 @@ import 'package:BMSrv/Models/JsonWrappers/REMetaData.dart';
 final scopes = [storage.StorageApi.DevstorageFullControlScope];
 
 @app.Group("/")
-class ImageService { 
+class ImageService {
   final log = new Logger("BMSrv.Services.ImageService");
   final String contentType = "image/jpeg";
   final String bucket = "semplex";
   final String googleBaseUrl = "http://storage.googleapis.com";
-  
+
   Uuid _Generator = new Uuid();
-  
+
   RealEstateService _estateSrv;
   Config _config;
-  
+
   auth.ServiceAccountCredentials accountCredentials = null;
   auth.AuthClient Client;
-  
+
   storage.StorageApi storageClient;
-  
+
   ImageService(RealEstateService this._estateSrv, Config this._config)
   {
     bool res = _config.hasSection('GoogleCloud');
     File file = new File(_config.get("GoogleCloud", "credentials"));
-    
+
     file.readAsString().then((String cont){
       accountCredentials = new auth.ServiceAccountCredentials.fromJson(cont);
       connect();
     });
   }
-  
+
   Future connect() async {
     try {
       Client  =  await auth_io.clientViaServiceAccount(accountCredentials, scopes);
@@ -54,9 +54,9 @@ class ImageService {
       log.warning(error);
     }
   }
-  
+
   Future<String> saveFile(@app.Body(app.FORM) var data,
-                          {String bucket : null, 
+                          {String bucket : null,
                            String prefix : ''}) async
   {
     if (data['file'] is app.HttpBodyFileUpload) {
@@ -79,7 +79,7 @@ class ImageService {
       throw new app.ErrorResponse(403, {"error": "data is not a file"});
     }
   }
-  
+
   @app.Route('/users/:id/profile/avatar', methods: const [app.POST],
              allowMultipartRequest: true)
   @ProtectedAccess(filtrateByUser: true)
@@ -91,11 +91,11 @@ class ImageService {
                                          bucket: bucketName,
                                          prefix: 'user-${user.id}-');
     final String publicUrl = "${googleBaseUrl}/${intUrl}";
-    
+
     if(user.profileImage != null) {
       try {
         Uri url = Uri.parse(user.profileImage);
-        
+
         var ret = await storageClient.objects.
             delete(bucketName, url.pathSegments.last);
       } catch(error) {
@@ -105,37 +105,39 @@ class ImageService {
     user.profileImage = publicUrl;
     return user.save();
   }
-  
+
   @app.Route('/realestate/:type/:id/rooms/:roomid/images/base',
              methods: const [app.POST], allowMultipartRequest: true)
   @ProtectedAccess(filtrateByUser: false, groups: const ['admin'])
-  addBaseImage(String type, String id, String roomid, 
+  addBaseImage(String type, String id, String roomid,
                @app.Body(app.FORM) var data) async
   {
     final String prefix = "main-$type-$id-$roomid-";
     final String intUrl = await saveFile(data, prefix: prefix);
     final String publicUrl = "${googleBaseUrl}/${intUrl}";
-    
+
     {
       Map<String, String> params = { 'value' : JSON.encode(publicUrl)};
       await _estateSrv.addDataForRoom(type, id, roomid, 'mainImageUrl', params);
     }
+
+    return publicUrl;
   }
-  
+
   @app.Route('/realestate/:type/:id/rooms/:roomid/images',
              methods: const [app.POST], allowMultipartRequest: true)
   @ProtectedAccess(filtrateByUser: false, groups: const ['user'])
-  addAdditionalImage(String type, String id, String roomid, 
+  addAdditionalImage(String type, String id, String roomid,
                      @app.Body(app.FORM) var data) async
   {
     REMetaDataWrapper images = await _estateSrv
                       .getDataForRoomByName(type, id, roomid, 'objectImages');
-    
+
     final String prefix = "additional-$type-$id-$roomid-";
     final String intUrl = await saveFile(data, prefix: prefix);
     final String publicUrl = "${googleBaseUrl}/${intUrl}";
     Map<String, String> params = null;
-    
+
     if(!images.data.containsKey('objectImages')) {
       params = { 'value' : JSON.encode([publicUrl])};
       await _estateSrv.
@@ -146,19 +148,19 @@ class ImageService {
       await _estateSrv.
         changeDataForRoom(type, id, roomid, 'objectImages', '0', params);
     }
-    
+
     return publicUrl;
   }
-  
+
   @app.Route('/realestate/:type/:id/rooms/:roomid/images',
              methods: const[app.GET])
-  getImages(String type, String id, String roomid) async 
+  getImages(String type, String id, String roomid) async
   {
     REMetaDataWrapper data = await _estateSrv
                   .getDataForRoomByName(type, id, roomid, 'objectImages');
-    
+
     if(data.data.containsKey('objectImages')) {
-      return data.data['objectImages'][0];  
+      return data.data['objectImages'][0];
     } else {
       return [];
     }
