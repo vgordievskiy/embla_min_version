fec284ec6cfe5893d64f4df52d59a3d6857e23de
Vladislav Gordievskiy <vladislav.gordievskiy@team.wrike.com>
2016-04-07 17:52:29 +0300

    improve

--- lib/Mail/Sender.dart
+++ lib/Mail/Sender.dart
@@ -8,40 +8,40 @@ import 'package:logging/logging.dart';
 import 'package:SrvCommon/SrvCommon.dart';
 
 
-class MailSender 
+class MailSender
 {
+  final log = new Logger("BMSrv.Mail.Sender");
+
   SmtpOptions options;
   SmtpTransport transport;
-  
+
   final String user;
   final String passwd;
   final String baseMail;
-  
+
   MailSender(this.user, this.passwd, [this.baseMail = null])
   {
     options = new YandexSmtpOptions();
     setUserName(this.user, passwd);
     transport = new SmtpTransport(options);
   }
-  
+
   void setUserName(String user, String passwd)
   {
     options..username = user
-      ..password = passwd; 
+      ..password = passwd;
   }
-  
+
   createActivateMail(String target, String subj, String html) {
     Envelope envelope = new Envelope();
     envelope.from = baseMail ?? user;
     envelope.recipients.add(target);
     envelope.subject = subj;
     envelope.html = html;
-    
-    transport.send(envelope)
-      .then((envelope) => print('Email sent!'))
-      .catchError((e) => print('Error occurred: $e'));
-  }
-  
-  
-}
 
+    transport.send(envelope)
+      .then((envelope) => log.info('Email sent!'))
+      .catchError((e) => log.log(Level.SEVERE, 'Error occurred: $e'));
+  }
+
+}
