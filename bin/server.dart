c66e7933e774c02f20113e02337676c0513dab69
Vladislav Gordievskiy <vladislav.gordievskiy@team.wrike.com>
2016-06-14 19:02:03 +0300

    improve

--- bin/server.dart
+++ bin/server.dart
@@ -7,7 +7,7 @@ import 'package:embla_trestle/embla_trestle.dart';
 export 'package:embla/application.dart';
 export 'package:embla/bootstrap.dart';
 
-import '../test_data/init_data.dart' as test;
+import '../test/test_data/init_data.dart' as test;
 
 var driver = new InMemoryDriver();
 
