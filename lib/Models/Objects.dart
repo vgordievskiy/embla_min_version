8c2921cceb01e86766806585558ae365e53c1b6e
v.gordievskiy <v.gordievskiy@gmail.com>
2016-06-18 22:46:47 +0300

    add EntityType

--- lib/Models/Objects.dart
+++ lib/Models/Objects.dart
@@ -1,6 +1,18 @@
 library tradem_srv.models.objects;
 import 'package:embla_trestle/embla_trestle.dart';
 
+class EntityType {
+  static final List<EntityType> values = [ EntityType.ROOM ];
+  static EntityType fromInt(int ind) => values[ind];
+  
+  final _value;
+  final int _intValue;
+  const EntityType._internal(this._value, this._intValue);
+  toString() => 'EntityType.$_value';
+
+  static const ROOM = const EntityType._internal('ROOM', 0);
+}
+
 class Entity extends Model {
   @field int id;
   @field int type;
