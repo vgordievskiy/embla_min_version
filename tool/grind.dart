733146afde21e68bb5a4834a0969b497b1c88219
Vladislav Gordievskiy <vladislav.gordievskiy@team.wrike.com>
2016-06-21 17:48:14 +0300

    add grind task for initialize postgis fields

--- tool/grind.dart
+++ tool/grind.dart
@@ -13,6 +13,15 @@ main(args) => grind(args);
 
 final gateway = new Gateway(driver);
 
+String getPostgresUri() {
+  final String username = config['username'];
+  final String password = config['password'];
+  final String database = config['database'];
+  final String host = 'localhost';
+  final int port = 5432;
+  return'postgres://$username:$password@$host:$port/$database';
+}
+
 @DefaultTask()
 migrate() async {
   await gateway.connect();
@@ -31,15 +40,32 @@ rollback() async {
 initpart() async {
   if(driver is PostgresqlDriver) {
     String script = await loadPartitionMagic();
-    final String username = config['username'];
-    final String password = config['password'];
-    final String database = config['database'];
-    final String host = 'localhost';
-    final int port = 5432;
-    String uri = 'postgres://$username:$password@$host:$port/$database';
-    postgresql.Connection con = await postgresql.connect(uri);
+    postgresql.Connection con = await postgresql.connect(getPostgresUri());
     int res = await con.execute(script);
     print("Create PartitionMagic: $res");
+    con.close();
+  } else {
+    print("driver is not PostgresqlDriver");
+  }
+}
+
+@Task()
+initpostgis() async {
+  if(driver is PostgresqlDriver) {
+    postgresql.Connection con = await postgresql.connect(getPostgresUri());
+    List<String> tables = [];
+    for(String table in tables) {
+      try {
+        await con.execute("SELECT AddGeometryColumn( '$table', 'obj_geom', -1, 'GEOMETRY', 2)");
+      } catch(err) {
+        print(err);
+      }
+      try {
+        await con.execute("CREATE INDEX ${table}_geom_indx ON $table USING GIST ( obj_geom )");
+      } catch(err) {
+        print(err);
+      }
+    }
     con.close();
   } else {
     print("driver is not PostgresqlDriver");
