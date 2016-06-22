9cc8abe58470a15db68bec299980a27db419e33e
v.gordievskiy <v.gordievskiy@gmail.com>
2016-06-23 00:13:34 +0300

    updt test

--- test/user_service_test.dart
+++ test/user_service_test.dart
@@ -1,6 +1,8 @@
 import "package:test/test.dart";
 import 'package:trestle/gateway.dart';
 import "package:embla/application.dart";
+import 'package:SemplexClientCmn/Utils/HttpCommunicator/IOHttpCommunicator.dart';
+import 'package:SemplexClientCmn/Utils/RestAdapter.dart';
 import 'package:tradem_srv/Services/UserService.dart';
 
 import '../bin/server.dart' as Srv;
@@ -13,17 +15,36 @@ main() async {
     List<Bootstrapper> bootstrappers = Srv.embla;
     app = await Application.boot(bootstrappers);
   });
-
   tearDown(() async {
     await app.exit();
   });
 
-  test(".split() splits the string on the delimiter", () {
-    expect("foo,bar,baz", allOf([
-      contains("foo"),
-      isNot(startsWith("bar")),
-      endsWith("baz")
-    ]));
-  });
+  group("int", () {
 
+    //IoHttpCommunicator cmn = new IoHttpCommunicator();
+    //RestAdapter rest;
+
+    setUp(() async {
+      print("!!!");
+    });
+    tearDown(() async {
+      print("222");
+    });
+
+    test(".split() splits the string on the delimiter", () {
+      expect("foo,bar,baz", allOf([
+        contains("foo"),
+        isNot(startsWith("bar")),
+        endsWith("baz")
+      ]));
+    });
+
+    test(".split() splits the string on the delimiter", () {
+      expect("foo,bar,baz", allOf([
+        contains("foo"),
+        isNot(startsWith("bar")),
+        endsWith("baz")
+      ]));
+    });
+  });
 }
