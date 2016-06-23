7a3826e5269da351087922f7883d6dc02272f8ff
Vladislav Gordievskiy <vladislav.gordievskiy@team.wrike.com>
2016-06-23 16:54:13 +0300

    draft for file upload

--- bin/server.dart
+++ bin/server.dart
@@ -4,6 +4,11 @@ import 'package:embla/http_basic_middleware.dart';
 import 'package:trestle/gateway.dart';
 import 'package:embla_trestle/embla_trestle.dart';
 
+import 'dart:convert';
+import 'dart:io';
+import 'package:http_server/src/http_body_impl.dart';
+import 'package:http_server/src/http_body.dart';
+
 export 'package:embla/application.dart';
 export 'package:embla/bootstrap.dart';
 
@@ -31,9 +36,41 @@ get embla => [
     pipeline: pipe(
       LoggerMiddleware, Srv.CORSMiddleware,
       Route.post('login/', Srv.JwtLoginMiddleware),
+      Route.post('test/', (Request req) async {
+        HttpBody tmp = await HttpBodyHandlerImpl.process(req.read(),
+          new _HttpHeaders(req.headers, ContentType.parse(req.headers['content-type'])), UTF8);
+        return tmp.type;
+      }),
       RemoveTrailingSlashMiddleware, InputParserMiddleware,
       Route.all('users/*', Srv.JwtAuthMiddleware, Srv.UserFilter, Srv.UserService)
     )
   ),
   new Srv.TrademSrv()
 ];
+
+class _HttpHeaders implements HttpHeaders {
+  final Map<String, String> headers;
+  ContentType _contentType;
+
+  _HttpHeaders(this.headers, this._contentType);
+
+  @override
+  List<String> operator [](String name) {
+    return headers[name].split(";");
+  }
+
+  @override
+  ContentType get contentType => _contentType;
+
+  @override
+  void forEach(void f(String name, List<String> values)) {
+    headers.forEach((k, v) => f(k, v.split(";")));
+  }
+
+  @override
+  String value(String name) {
+    return headers[name];
+  }
+
+  dynamic noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
