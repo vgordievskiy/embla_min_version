d472e04e4f842443e13c9527cd86ae621cde32ef
v.gordievskiy <v.gordievskiy@gmail.com>
2016-06-24 00:26:24 +0300

    updt

---
+++ lib/Utils/Crypto.dart
@@ -0,0 +1,11 @@
+library tradem_srv.utils.crypro;
+
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
+
+
+String encryptPassword(String pass) {
+  var toEncrypt = new SHA1();
+  toEncrypt.add(UTF8.encode(pass));
+  return CryptoUtils.bytesToHex(toEncrypt.close());
+}
