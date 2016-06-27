e842e5cecf6e2929b69fa43177088a333560dde8
Vladislav Gordievskiy <vladislav.gordievskiy@team.wrike.com>
2016-06-27 16:32:07 +0300

    add ObjectService

---
+++ lib/Services/ObjectService.dart
@@ -0,0 +1,19 @@
+library tradem_srv.services.object_service;
+
+import 'dart:async';
+import 'dart:convert';
+import 'package:embla/http.dart';
+import 'package:embla/http_annotations.dart';
+import 'package:embla_trestle/embla_trestle.dart';
+
+import '../Utils/Utils.dart';
+import '../Models/Objects.dart';
+import '../Middleware/input_parser/input_parser.dart';
+
+
+class ObjectService extends Controller {
+  final Repository<Entity> entities;
+
+  ObjectService(this.entities);
+
+}
