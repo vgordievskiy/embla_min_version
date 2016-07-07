70bf623c6f6f09d5ab635f645ca38a3e34e6041a
Vladislav Gordievskiy <vladislav.gordievskiy@team.wrike.com>
2016-07-07 18:01:52 +0300

    init utils

---
+++ lib/Utils/Deals.dart
@@ -0,0 +1,11 @@
+library tradem_srv.utils.deals;
+
+import 'package:srv_base/Models/Users.dart';
+import '../Models/Deals.dart';
+import '../Models/Objects.dart';
+
+export '../Models/Deals.dart';
+
+class DealsUtils {
+  
+}
