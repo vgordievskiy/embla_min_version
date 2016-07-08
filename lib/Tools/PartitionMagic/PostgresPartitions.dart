4c7098d342d7d2b3c93b788be3ca89ad49a9435b
Vladislav Gordievskiy <vladislav.gordievskiy@team.wrike.com>
2016-07-08 19:38:36 +0300

    fix

--- lib/Tools/PartitionMagic/PostgresPartitions.dart
+++ lib/Tools/PartitionMagic/PostgresPartitions.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 import 'dart:async';
 
 final String _relativePath =
-  "lib/Tools/PartitionMagic/SqlScript/_2gis_partition_magic.sql";
+  "packages/srv_base/Tools/PartitionMagic/SqlScript/_2gis_partition_magic.sql";
 
 Future<String> loadPartitionMagic() async {
   File file = new File(_relativePath);
