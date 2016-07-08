d533ba52da9005f0003df192004d7398d6c70cdc
v.gordievskiy <v.gordievskiy@gmail.com>
2016-07-09 00:00:35 +0300

    update

--- lib/Models/Prices.dart
+++ lib/Models/Prices.dart
@@ -3,16 +3,14 @@ import 'package:embla_trestle/embla_trestle.dart';
 
 class Price extends Model {
   @field int id;
-  @field DateTime created_at;
-  @field DateTime updated_at;
   @field int entity_id;
   @field double price;
 
   Map toJson() {
     return {
       'id' : id,
-      'created_at' : created_at,
-      'updated_at' : updated_at,
+      'created_at' : createdAt,
+      'updated_at' : updatedAt,
       'entity_id'  : entity_id,
       'price'      : price
     };
