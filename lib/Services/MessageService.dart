f3409110620d82a34499b8090199c436713a1fdb
Vladislav Gordievskiy <vladislav.gordievskiy@team.wrike.com>
2016-07-11 18:48:39 +0300

    init MessageService

---
+++ lib/Services/MessageService.dart
@@ -0,0 +1,20 @@
+library tradem_srv.services.message_service;
+
+import 'dart:async';
+import 'package:embla/http.dart';
+import 'package:embla/http_annotations.dart';
+import 'package:embla_trestle/embla_trestle.dart';
+import 'package:harvest/harvest.dart';
+import '../Events/UserEvents.dart';
+
+import 'package:srv_base/Utils/Utils.dart';
+import 'package:srv_base/Utils/Crypto.dart' as crypto;
+import 'package:srv_base/Middleware/input_parser/input_parser.dart';
+import 'package:srv_base/Models/Users.dart';
+import '../Utils/Deals.dart';
+import '../Utils/Prices.dart';
+
+export 'package:srv_base/Models/Users.dart';
+
+class MessageService extends Controller {
+}
