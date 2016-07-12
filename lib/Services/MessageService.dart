36672b93d403e380b3248e776e9221650c45610f
Vladislav Gordievskiy <vladislav.gordievskiy@team.wrike.com>
2016-07-12 12:49:57 +0300

    add mustache template

--- lib/Services/MessageService.dart
+++ lib/Services/MessageService.dart
@@ -3,6 +3,7 @@ library tradem_srv.services.message_service;
 import 'package:embla/http.dart';
 import 'package:embla/http_annotations.dart';
 import 'package:harvest/harvest.dart';
+import 'package:mustache/mustache.dart';
 import '../Events/UserEvents.dart';
 
 import 'package:srv_base/Utils/Utils.dart';
@@ -12,10 +13,15 @@ import 'package:logging/logging.dart';
 
 export 'package:srv_base/Models/Users.dart';
 
+Map<String, String> templates = {
+  'new-user' : '''<h2>Привет</h2><div>{{ name }}</div>'''
+};
+
 class MessageService extends Controller {
   final log = new Logger("tradem.Services.MessageService");
   MessageBus _bus;
-  MailSender mail = new MailSender('service@semplex.ru', 'SSemplex!2#');
+  //MailSender mail = new MailSender('service@semplex.ru', 'SSemplex!2#');
+  MailSender mail = new MailSender('v.gordievskiy@semplex.ru', 'bno9mjc');
 
   MessageService()
   {
@@ -26,12 +32,9 @@ class MessageService extends Controller {
   }
 
   newUser(User user) {
+    Template mailBody = new Template(templates['new-user']);
     String subj = "Добро пожаловать в мир умных инвестиций";
-    String html =
-      '''<h4>
-          Привет
-         </h4>
-      ''';
+    String html = mailBody.renderString({ 'name' : user.email });
     mail.sendMail(new TMessage(subj, html, [user.email]));
     log.info("send welcome email");
   }
